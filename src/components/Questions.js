c952b81bbe595f9edfcf518c6eeaf682f3d2f13e
Lucas Lafèré <laferelucas@gmail.com>
2022-05-14 12:56:56 -0300

    mensagens de resultado aparecem e funcionam corretamente. Falta fazer os icones de todas as perguntas respondidas aparecerem, nao apenas a ultima. Alem disso, faltam pequenos fix (componentizar mais, proibir de abrir 2 perguntas)

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,14 +6,19 @@ import React from "react"
 
 export default function Questions ({result, setResult}) {
 
-    let newResult = [...result]
+    let newResult = [...result];
+    
+    let iconsArray = [];
     const [type, setType] = React.useState("question")
+    const [mistakes, setMistakes] = React.useState([])
+    let newMistakes = [...mistakes];
 
     arrayQuestions.sort(randomize)
     return (
         <>
-            {arrayQuestions.map((el, index) => <EachQuestion question={el.question} answer={el.answer} key={index} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType}/>)}
-            <div className="footer">{newResult.length}/8 CONCLUÍDOS</div>
+            {arrayQuestions.map((el, index) => <EachQuestion question={el.question} answer={el.answer} key={index} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray} newMistakes={newMistakes} setMistakes={setMistakes}/>)}
+
+            <Footer  result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} iconsArray={iconsArray} newMistakes={newMistakes} setMistakes={setMistakes}/>
         </>
     )
 }
@@ -40,10 +45,10 @@ function randomize () {
 
 //Questions
 
-function EachQuestion ({question, answer, index, result, setResult, newResult, type, setType}) {
+function EachQuestion ({question, answer, index, result, setResult, newResult, type, setType, mistakes, iconsArray, newMistakes, setMistakes}) {
     const [start, setStart] = React.useState("")
     React.useEffect(() => {
-        setStart(<QuestionBox setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType}/>)
+        setStart(<QuestionBox setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray} newMistakes={newMistakes} setMistakes={setMistakes}/>)
       }, [])
     
     
@@ -55,35 +60,37 @@ function EachQuestion ({question, answer, index, result, setResult, newResult, t
     )
 }
 
-function QuestionBox ({question, answer, index, result, setResult, start, setStart, newResult, type, setType, el}) {
+function QuestionBox ({question, answer, index, result, setResult, start, setStart, newResult, type, setType, el, mistakes, iconsArray, newMistakes, setMistakes}) {
 
     function openQuestion () {
-        setStart(<QuestionOpen setStart={setStart} start ={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} />)     
+        setStart(<QuestionOpen setStart={setStart} start ={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray} newMistakes={newMistakes} setMistakes={setMistakes} />)     
     }
-    console.log(type)
 
     if (el === "question wrong") {
+        iconsArray.push(el)
         return (
             <div className={el}>
                 <h2>Pergunta {index+1}</h2>
-                <ion-icon name="play-outline" onClick={openQuestion}></ion-icon>
+                <ion-icon name="close-circle" className="red"></ion-icon>
             </div>
         )
     }
     else if (el === "question middle"){
+        iconsArray.push(el)
         return (
             <div className={el}>
                 <h2>Pergunta {index+1}</h2>
-                <ion-icon name="play-outline" onClick={openQuestion}></ion-icon>
+                <ion-icon name="help-circle" className="orange"></ion-icon>
             </div>
         )
     }
 
     else if (el === "question correct"){
+        iconsArray.push(el)
         return (
             <div className={el}>
                 <h2>Pergunta {index+1}</h2>
-                <ion-icon name="play-outline" onClick={openQuestion}></ion-icon>
+                <ion-icon name="checkmark-circle" className="green"></ion-icon>
             </div>
         )
     }
@@ -93,16 +100,16 @@ function QuestionBox ({question, answer, index, result, setResult, start, setSta
             
             <div className="question">
                 <h2>Pergunta {index+1}</h2>
-                <ion-icon name="play-outline" onClick={openQuestion}></ion-icon>
+                <ion-icon name="play-outline" className="black" onClick={openQuestion}></ion-icon>
             </div>
     )
 }
 
 
-function QuestionOpen ({question, answer, index, result, setResult, start, setStart, newResult, type, setType}) {
+function QuestionOpen ({question, answer, index, result, setResult, start, setStart, newResult, type, setType, mistakes, iconsArray, newMistakes, setMistakes}) {
 
     function openAnswer () {
-        setStart(<QuestionAnswer setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType}/>)
+        setStart(<QuestionAnswer setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray} newMistakes={newMistakes} setMistakes={setMistakes}/>)
     }
 
     return (
@@ -115,31 +122,113 @@ function QuestionOpen ({question, answer, index, result, setResult, start, setSt
 
 
 
-function QuestionAnswer ({question, answer, index, result, setResult, start, setStart, newResult, type, setType}) {
+function QuestionAnswer ({question, answer, index, result, setResult, start, setStart, newResult, type, setType, mistakes, iconsArray, newMistakes, setMistakes}) {
 
 
     return (
             <div className="question-answer">
                 <h2>{answer}</h2>
-                <Buttons setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType}/>
+                <Buttons setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray} newMistakes={newMistakes} setMistakes={setMistakes}/>
             </div>
     )
 }
 
-function Buttons ({question, answer, index, result, setResult, start, setStart, newResult, type, setType}) {
+function Buttons ({question, answer, index, result, setResult, start, setStart, newResult, type, setType, mistakes, iconsArray, newMistakes, setMistakes}) {
 
-    function CloseAnswer (el) {
-        setStart(<QuestionBox setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} el={el}/>)  
-
+    function closeAnswer (el) {
+        setStart(<QuestionBox setStart={setStart} start={start} question={question} answer={answer} index={index} result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} el={el} mistakes={mistakes} iconsArray={iconsArray} newMistakes={newMistakes} setMistakes={setMistakes}/>)  
+        setType(el)
         newResult.push(1)
         setResult([...newResult])
+        console.log(mistakes.length)
+
+        if (el === 'question wrong'){
+            newMistakes.push(1)
+            setMistakes([...newMistakes])
+        }
+
     }
+
+
 
     return (
         <div className="button-container">
-            <div className="button-answer red" onClick={() => CloseAnswer('question wrong')}><p>Não Lembrei</p></div>
-            <div className="button-answer orange" onClick={() => CloseAnswer('question middle')}><p>Quase não lembrei</p></div>
-            <div className="button-answer green" onClick={() => CloseAnswer('question correct')}><p>Zap!</p></div>
+            <div className="button-answer red" onClick={() => closeAnswer('question wrong')}><p>Não Lembrei</p></div>
+            <div className="button-answer orange" onClick={() => closeAnswer('question middle')}><p>Quase não lembrei</p></div>
+            <div className="button-answer green" onClick={() => closeAnswer('question correct')}><p>Zap!</p></div>
+        </div>
+    )
+}
+
+function Icons ({question, answer, index, result, setResult, start, setStart, newResult, type, setType, mistakes, iconsArray, newMistakes, setMistakes}) {
+
+    if (type === "question wrong") {
+        return (
+            <ion-icon name="close-circle" style= {{color: "red"}}></ion-icon>
+        )
+    }
+    else if (type === "question middle") {
+        return (
+            <ion-icon name="help-circle" style= {{color: "orange"}}></ion-icon>
+
+        )
+    }
+    else if (type === "question correct") {
+        return (
+            <ion-icon name="checkmark-circle" style= {{color: "green"}}></ion-icon>
+        )
+    }
+
+    return (
+        <></>
+    )
+}
+
+
+function Footer ({result, setResult, newResult, type, setType, mistakes, iconsArray, newMistakes, setMistakes}) {
+    console.log(newMistakes.length)
+
+    if (newResult.length === 8 && newMistakes.length === 0) {
+        return (
+            <div className="footer-big">
+                <div className="result-class">
+                    <img src="./Assets/party.png" alt="" />
+                    <h1>Parabéns!</h1>
+                </div>
+
+                <h3>Você não esqueceu de nenhum flashcard!</h3>
+                <h2>{newResult.length}/8 CONCLUÍDOS</h2>
+                <div className="icons">
+                    {<Icons result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray}/>}
+                </div>
+            </div>
+        )
+    }
+    else if (newResult.length === 8 && newMistakes.length > 0) {
+        return (
+            <div className="footer-big">
+                <div className="result-class">
+                    <img src="./Assets/sad.png" alt="" />
+                    <h1>Putz...</h1>
+                </div>
+                <h3>Ainda faltam alguns...
+                    <br></br>
+                    Mas não desanime!
+                </h3>
+                <h3></h3>
+                <h2>{newResult.length}/8 CONCLUÍDOS</h2>
+                <div className="icons">
+                    {<Icons result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray} />}
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className="footer">
+            <h2>{newResult.length}/8 CONCLUÍDOS</h2>
+            <div className="icons">
+                {<Icons result={result} setResult={setResult} newResult={newResult} type={type} setType={setType} mistakes={mistakes} iconsArray={iconsArray}/>}
+            </div>
         </div>
     )
 }
