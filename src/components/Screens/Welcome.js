6f31d67dded8a0f4f14d021080d870c3d9da55eb
Lucas Lafèré <laferelucas@gmail.com>
2022-05-16 17:04:03 -0300

    add audio no onclick de iniciar

--- src/components/Screens/Welcome.js
+++ src/components/Screens/Welcome.js
@@ -1,11 +1,15 @@
 import React from "react"
 import logo from "../../Assets/logo.png"
+import audio from "../../Assets/Clash-clash.mp3"
 
 
-export default function Welcome (props) {
-    
+export default function Welcome (props) { 
+    const audioElement = new Audio()
+    audioElement.src = audio
+    audioElement.volume = 0.1
     function changeScreen () {
         props.setScreen("content")
+        audioElement.play()
     }
 
 
